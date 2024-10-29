d9bcdd453468aa2424965f1462a2a1ef67bc13c9
Ben Stoica <bstoica@rpmmoves.com>
2024-10-29 10:15:36 -0400

    ready to publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve, extname, relative } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { glob } from "glob";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ["lib"],
+      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/**/*.{ts, tsx}", {
+            ignore: ["lib/**/*.d.ts"],
+          })
+          .map((file) => [
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
